3a35beda25c59ca341f7995e3ffe46baa4836ea3
Akshay Soni <akshay@visibly.io>
2020-03-15 13:48:29 +0530

    first commit

--- app/Users.js
+++ app/Users.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import history from './history';
+
+
 
 export default class Users extends React.Component {
     constructor(props) {
@@ -7,39 +10,39 @@ export default class Users extends React.Component {
             data: [{
                 id: 1,
                 name: "Island",
-                image: "https://images.unsplash.com/photo-1442530792250-81629236fe54?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=9631adb2d2f752e3a0734f393fef634b"
-            }, {
+                image: "https://img.icons8.com/clouds/2x/user.png",
                 id: 2,
-                name: "Forest",
-                image: "https://images.unsplash.com/photo-1468851508491-4f854ec88aa0?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=b1222b6a1d3694cac76d2a23c3a02254"
+                name: "jack",
+                image: "https://img.icons8.com/clouds/2x/user.png",
+
             }, {
                 id: 3,
-                name: "Whale",
-                image: "https://images.unsplash.com/photo-1454991727061-be514eae86f7?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=3c55430f01fe9ac9a9ccb3383d1416ff"
+                name: "jack",
+                image: "https://img.icons8.com/clouds/2x/user.png",
             }, {
                 id: 4,
-                name: "Mountain",
-                image: "https://images.unsplash.com/photo-1467890947394-8171244e5410?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=9396f0adf263b51b44626228225684d0"
+                name: "jack",
+                image: "https://img.icons8.com/clouds/2x/user.png",
             }, {
                 id: 5,
-                name: "Boat",
-                image: "https://images.unsplash.com/photo-1443302382600-0bfacc473376?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=0c0f26518c1001f67b6c2e4480a8d3e0"
+                name: "jack",
+                image: "https://img.icons8.com/clouds/2x/user.png",
             }, {
                 id: 6,
-                name: "Flowers",
-                image: "https://images.unsplash.com/photo-1429091443922-e7d9ae79a837?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=e81cb6a60c53788559edb9bec21b80fc"
+                name: "jack",
+                image: "https://img.icons8.com/clouds/2x/user.png",
             }, {
                 id: 7,
-                name: "Fire",
-                image: "https://images.unsplash.com/photo-1468245856972-a0333f3f8293?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=1f57cc13084e32839627453821a43abf"
+                name: "jack",
+                image: "https://img.icons8.com/clouds/2x/user.png",
             }, {
                 id: 8,
-                name: "Garden",
-                image: "https://images.unsplash.com/photo-1427392797425-39090deb14ec?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=8bfe49466d0da200e61128a8ab0e8fbe"
+                name: "jack",
+                image: "https://img.icons8.com/clouds/2x/user.png",
             }, {
                 id: 9,
-                name: "Bridge",
-                image: "https://images.unsplash.com/photo-1445723356089-6dbb51d9c4f8?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=6e476c6e7ce1adac161295616d1bec05"
+                name: "jack",
+                image: "https://img.icons8.com/clouds/2x/user.png",
             }]
         };
     }
@@ -67,6 +70,7 @@ class Tiles extends React.Component {
 }
 
 class Tile extends React.Component {
+
 	constructor(props) {
 			super(props);
 			this.state = {
@@ -98,16 +102,9 @@ class Tile extends React.Component {
 	}
 	_clickHandler(e) {
         
-		// e.preventDefault();
-		// if (this.state.open === false) {
-		// 	this.setState({
-		// 		open: true
-		// 	});
-		// } else {
-		// 	this.setState({
-		// 		open: false
-		// 	});
-		// }
+        e.preventDefault();
+        history.push('/photos');
+	
 	}
 
 	render() {
@@ -138,7 +135,7 @@ class Tile extends React.Component {
 		}
 
 		return (
-			<div className="tile">
+			<div className="Usertile">
 				<img
 					onMouseEnter={this._mouseEnter}
 					onMouseLeave={this._mouseLeave}
@@ -147,6 +144,7 @@ class Tile extends React.Component {
 					alt={this.props.data.name}
 					style={tileStyle}
 				/>
+                <div className="username">{this.props.data.name} </div> 
 			</div>
 		);
 	}
